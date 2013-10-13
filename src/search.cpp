b630e694692dd26ec413a9a0161b3bea5eca6288
Johannes Kirschner <mail@jkirschner.org>
2013-10-13 08:25:39 +0200

    moving class structure to header file

--- src/search.cpp
+++ src/search.cpp
@@ -5,49 +5,11 @@
 
 #include <cmath>
 
-typedef unsigned long long visits_t;
-
 
 // search options
 static const visits_t     MinVisitsBeforeExpansion = 1;
 static const unsigned int MaxDistanceFromRoot  = 100;// Note, Agent defines it's own horizon size;
 static size_t             MaxSearchNodes;
-
-// contains information about a single "state"
-class SearchNode {
-
-public:
-
-	SearchNode(bool is_chance_node, unsigned int num_actions,
-	    unsigned int num_percepts);
-    
-    ~SearchNode();
-    
-	// determine the next action to play
-	action_t selectAction(Agent &agent, unsigned int dfr); // TODO: implement
-
-	// determine the expected reward from this node
-	reward_t expectation(void) const { return m_mean; }
-
-	// perform a sample run through this node and it's children,
-	// returning the accumulated reward from this sample run
-	reward_t sample(Agent &agent, unsigned int dfr); // TODO: implement
-
-	// number of times the search node has been visited
-	visits_t visits(void) const { return m_visits; }
-	
-	SearchNode* child(unsigned int n) { return m_child[n]; }
-
-private:
-
-	bool m_chance_node; // true if this node is a chance node, false otherwise
-	double m_mean;      // the expected reward of this node
-	visits_t m_visits;  // number of times the search node has been visited
-
-	// TODO: decide how to reference child nodes
-	//  e.g. a fixed-size array
-	std::vector<SearchNode*> m_child;
-};
 
 SearchNode::SearchNode(bool is_chance_node,
     unsigned int num_actions, unsigned int num_percepts)
